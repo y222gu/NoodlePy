327d04c23b7440484e51f1ad00b72543def7a6c1
Yifei Gu <yifeigu@yahoo.com>
2024-04-01 23:21:40 -0700

    cnn backbone

--- noodlepy/ml/simsiam_model.py
+++ noodlepy/ml/simsiam_model.py
@@ -6,23 +6,50 @@ import pytorch_lightning as pl
 import torch
 import torchvision
 from torch import nn
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
 from lightly.loss import NegativeCosineSimilarity
 from lightly.models.modules import SimSiamPredictionHead, SimSiamProjectionHead
-from lightly.transforms import SimSiamTransform
+
+from noodlepy.ml.raman_dataset import RamanDataset
+
+class cnn_backbone(nn.Module):
+    def __init__(self, layer_channel_sizes):
+        super(cnn_backbone, self).__init__()
+        layers = []
+        in_channels = layer_channel_sizes[0] #intialize the input channel size with the first element of the list
+
+        for out_channels in layer_channel_sizes[1:-1]:
+            conv_layer = nn.Conv1d(in_channels, out_channels, kernel_size=3, padding=1)
+            torch.nn.init.kaiming_uniform_(conv_layer.weight, nonlinearity='relu') # weights initialization using kaiming uniform
+            layers.append(conv_layer)
+            layers.append(nn.ReLU())
+            layers.append(nn.AvgPool1d(kernel_size=2)) # layers.append(nn.MaxPool1d(kernel_size=2))
+            in_channels = out_channels
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.layers(x)
+        return x
+
+class resnet_backbone():
+    resnet = torchvision.models.resnet18()
+    backbone = nn.Sequential(*list(resnet.children())[:-1])
 
 
 class SimSiam(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, backbone):
         super().__init__()
-        resnet = torchvision.models.resnet18()
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
-        self.projection_head = SimSiamProjectionHead(512, 512, 128)
-        self.prediction_head = SimSiamPredictionHead(128, 64, 128)
+        self.backbone = backbone
+        self.projection_head = SimSiamProjectionHead(128, 128, 64)
+        self.prediction_head = SimSiamPredictionHead(64, 32, 64)
         self.criterion = NegativeCosineSimilarity()
 
     def forward(self, x):
         f = self.backbone(x).flatten(start_dim=1)
+        print(f.shape)
         z = self.projection_head(f)
         p = self.prediction_head(z)
         z = z.detach()
@@ -39,25 +66,19 @@ class SimSiam(pl.LightningModule):
         optim = torch.optim.SGD(self.parameters(), lr=0.06)
         return optim
 
+if __name__ == "__main__":
+    cnn_backbone_1d = cnn_backbone([1, 8, 16, 32, 64, 128]) # 1D spectral data start with 1 channel, RGB 2D image start with 3 channels
+    model = SimSiam(cnn_backbone_1d)
 
-model = SimSiam()
+    dataset = RamanDataset()
+    dataloader = DataLoader(
+        dataset,
+        batch_size=4,
+        shuffle=True,
+        drop_last=True,
+        num_workers=8,
+    )
+    accelerator = "gpu" if torch.cuda.is_available() else "cpu"
 
-transform = SimSiamTransform(input_size=32)
-dataset = torchvision.datasets.CIFAR10(
-    "datasets/cifar10", download=True, transform=transform
-)
-# or create a dataset from a folder containing images or videos:
-# dataset = LightlyDataset("path/to/folder", transform=transform)
-
-dataloader = torch.utils.data.DataLoader(
-    dataset,
-    batch_size=256,
-    shuffle=True,
-    drop_last=True,
-    num_workers=8,
-)
-
-accelerator = "gpu" if torch.cuda.is_available() else "cpu"
-
-trainer = pl.Trainer(max_epochs=10, devices=1, accelerator=accelerator)
-trainer.fit(model=model, train_dataloaders=dataloader)
+    trainer = pl.Trainer(max_epochs=1, devices=1, accelerator=accelerator)
+    trainer.fit(model=model, train_dataloaders=dataloader)
