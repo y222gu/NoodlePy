5dc5f4cbb9b19ee92bcdd8f3278c0d7d5c28b806
Yifei Gu <yifeigu@yahoo.com>
2024-04-15 10:47:12 -0700

    Add train dataset and test dataset.
    Train the model, and extract the embedding for the test set and visualize it with t-sne

--- noodlepy/ml/simsiam_model.py
+++ noodlepy/ml/simsiam_model.py
@@ -7,12 +7,15 @@ import torch
 import torchvision
 from torch import nn
 from torch.utils.data import DataLoader
-import torch.nn.functional as F
-
+import os
 from lightly.loss import NegativeCosineSimilarity
 from lightly.models.modules import SimSiamPredictionHead, SimSiamProjectionHead
-
 from noodlepy.ml.raman_dataset import RamanDataset
+import wandb
+import math
+from sklearn.manifold import TSNE
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class cnn_backbone(nn.Module):
     def __init__(self, layer_channel_sizes):
@@ -72,44 +75,122 @@ class SimSiam(pl.LightningModule):
 
 
 if __name__ == "__main__":
-    cnn_backbone_1d = cnn_backbone([1, 8, 16, 32, 64, 128]) # 1D spectral data start with 1 channel, RGB 2D image start with 3 channels
-    model = SimSiam(cnn_backbone_1d)
 
-    dataset = RamanDataset()
-    dataloader = DataLoader(
-        dataset,
-        batch_size=10,
+    wandb.login()
+    wandb.init(
+        # Set the project where this run will be logged
+        project="test", 
+        # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
+        name=f"experiment_{1}", 
+        # Track hyperparameters and run metadata
+        config={
+            "learning_rate": 0.02,
+            "epochs": 1,
+            "batch_size": 10,
+            "backbone_dim": [1, 8, 16, 32, 64, 128]
+            })
+    training_cfg = wandb.config
+    
+
+    cnn_backbone_1d = cnn_backbone(training_cfg.backbone_dim) # 1D spectral data start with 1 channel, RGB 2D image start with 3 channels
+    model = SimSiam(cnn_backbone_1d)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    criterion = NegativeCosineSimilarity()
+    optimizer = torch.optim.SGD(model.parameters(), lr=training_cfg.learning_rate)
+
+    current_directory = os.getcwd()
+    train_dataset_path = os.path.join(current_directory, "noodlepy", "data", "Raman_DB", "train")
+    test_dataset_path = os.path.join(current_directory, "noodlepy", "data", "Raman_DB", "test")
+
+    train_dataset = RamanDataset(train_dataset_path)
+    test_dataset = RamanDataset(test_dataset_path)
+
+    train_dataloaders = DataLoader(
+        train_dataset,
+        batch_size=training_cfg.batch_size,
         shuffle=True,
         drop_last=True,
         num_workers=8,
     )
-
-    torch.set_float32_matmul_precision("medium")
-    accelerator = "gpu" if torch.cuda.is_available() else "cpu"
-    trainer = pl.Trainer(max_epochs=1, devices=1, accelerator=accelerator)
-    trainer.fit(model=model, train_dataloaders=dataloader)
-
-
-    '''
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model.to(device)
-    criterion = NegativeCosineSimilarity()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.06
+    test_dataloaders = DataLoader(
+        test_dataset,
+        batch_size=training_cfg.batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=8,
+    )
 
     print("Starting Training")
-    for epoch in range(2):
-        total_loss = 0
-        for batch in dataloader:
+    for epoch in range(10):
+        avg_loss = 0.0
+        avg_output_std = 0.0
+        #total_loss = 0.0
+        for i, batch in enumerate(train_dataloaders, 0):
             x0, x1 = batch
             x0 = x0.to(device)
             x1 = x1.to(device)
             z0, p0 = model(x0)
             z1, p1 = model(x1)
             loss = 0.5 * (criterion(z0, p1) + criterion(z1, p0))
-            total_loss += loss.detach()
+            #total_loss += loss.detach()
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-        avg_loss = total_loss / len(dataloader)
-        print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
-'''
+
+            # calculate the per-dimension standard deviation of the outputs
+            # we can use this later to check whether the embeddings are collapsing
+            output = p0.detach()
+            output = torch.nn.functional.normalize(output, dim=1)
+            output_std = torch.std(output, 0)
+            output_std = output_std.mean()
+            # use moving averages to track the loss and standard deviation
+            w = 0.9
+            avg_loss = w * avg_loss + (1 - w) * loss.item()
+            avg_output_std = w * avg_output_std + (1 - w) * output_std.item()
+
+        # the level of collapse is large if the standard deviation of the l2
+        # normalized output is much smaller than 1 / sqrt(dim)
+        collapse_level = max(0.0, 1 - math.sqrt(64) * avg_output_std)
+        wandb.log({'epoch': epoch+1, 'loss': avg_loss, 'collapse_level': collapse_level})
+        print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}, collapse_level: {collapse_level:.5f}")
+
+    print("Finished Training")
+
+    # Extract embedding of the test set
+    embeddings = []
+    filenames = []
+    # disable gradients for faster calculations
+    model.eval()
+    with torch.no_grad():
+        for i,(x,_) in enumerate(test_dataloaders):
+            # embed the images with the pre-trained backbone
+            x = x.to(device)
+            y = model.backbone(x).flatten(start_dim=1)
+            # store the embeddings in a list
+            embeddings.append(y)
+
+    # concatenate the embeddings and convert to numpy
+    embeddings = torch.cat(embeddings, dim=0)
+    embeddings = embeddings.cpu().numpy()
+
+    # visualize the embeddings with t-SNE
+    tsne = TSNE(random_state = 0, n_iter = 1000, metric = 'cosine')
+    # Fit and transform
+    embeddings2d = tsne.fit_transform(embeddings)
+    # Create DF
+    embeddingsdf = pd.DataFrame()
+    # Add x coordinate
+    embeddingsdf['x'] = embeddings2d[:,0]
+    # Add y coordinate
+    embeddingsdf['y'] = embeddings2d[:,1]
+    # Check
+    embeddingsdf.head()
+    # Set figsize
+    fig, ax = plt.subplots(figsize=(10,8))
+    # Scatter points, set alpha low to make points translucent
+    ax.scatter(embeddingsdf.x, embeddingsdf.y, alpha=.1)
+    plt.title('Scatter plot of games using t-SNE')
+    plt.show()
+    save_path = os.path.join(current_directory, "output_plots", "tsne_plot.png")
+    plt.savefig(save_path)
