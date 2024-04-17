8f1e652cdefc34ba864a6e1fc126c4619f6df391
y222gu <54646726+y222gu@users.noreply.github.com>
2024-04-17 13:27:40 -0700

    added new metadata to each spectrum in the spectrum class

--- noodlepy/utils/spectrum_class.py
+++ noodlepy/utils/spectrum_class.py
@@ -14,9 +14,14 @@ class Spectrum:
                  patient_id:str ='',
                  sample_type:str ='',
                  spectrum_id:str ='', 
-                 laser_wavelength_nm:float = [],
+                 laser_wavelength_nm:float = 785, #TODO: this should be loaded from the experimental metadata
                  wavelength_nm:np.array =[],
-                 intensity:np.array =[]):
+                 intensity:np.array =[],
+                 staging:int = None,
+                 age:int = None,
+                 gender:str = None,
+                 race:str = None,
+                 bim:float = None):
 
         self.patient_id:str = patient_id
         self.sample_type:str = sample_type
@@ -25,6 +30,11 @@ class Spectrum:
         self.wavelength_nm:np.array = wavelength_nm.round(3)
         self.raman_shift_cm:np.array = Spectrum.wavelength_to_raman_shift(self.wavelength_nm, self.laser_wavelength_nm)
         self.intensity:np.array = intensity
+        self.staging = staging
+        self.age = age
+        self.gender = gender
+        self.race = race
+        self.bim = bim
         
     def __len__(self):
         return len(self.intensity)
