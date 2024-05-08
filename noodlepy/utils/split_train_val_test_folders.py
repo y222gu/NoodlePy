115062890c074ac90d147c2a70aabeec8d559f14
Yifei Gu <yifeigu@yahoo.com>
2024-05-08 13:26:17 -0700

    script to split train, val, and test sets

--- noodlepy/utils/split_train_val_test_folders.py
+++ noodlepy/utils/split_train_val_test_folders.py
@@ -3,14 +3,14 @@ import splitfolders
 print("Splitting started.")
 
 # Define the input folder containing all the files
-input_folder = r"C:\Users\Yifei\Documents\NoodlePy\noodlepy\data\Raman_DB"
+input_folder = r"/mnt/c/Users/Yifei/Documents/NoodlePy/noodlepy/data/Raman_DB/plasma/separate_by_stage"
 
 # Define the output folder where train, validation, and test sets will be created
-output_folder = r"C:\Users\Yifei\Documents\NoodlePy\noodlepy\data\Raman_DB"
+output_folder = r"/mnt/c/Users/Yifei/Documents/NoodlePy/noodlepy/data/Raman_DB/plasma/separate_by_stage"
 
 # Define the split ratios (e.g., 80% train, 10% validation, 10% test)
 # You can adjust these ratios according to your requirements
-split_ratio = (0.8, 0.1, 0.1)  # Train, Validation, Test
+split_ratio = (0.7, 0.2, 0.1)  # Train, Validation, Test
 
 # Perform the split
 splitfolders.ratio(input_folder, output=output_folder, seed=88, ratio=split_ratio, group_prefix=None)
