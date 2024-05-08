9da92b86b9bd2caf034730a5b8dec6c39860da45
Yifei Gu <yifeigu@yahoo.com>
2024-05-08 15:48:49 -0700

    update the name for the default config name

--- noodlepy/utils/class_SpectrumPreprocessor.py
+++ noodlepy/utils/class_SpectrumPreprocessor.py
@@ -14,7 +14,7 @@ class SpectrumPreprocessor:
         
         if config_path is None:
             current_directory = os.getcwd()
-            config_path = os.path.join(current_directory, "noodlepy", "config", "config.yml")
+            config_path = os.path.join(current_directory, "noodlepy", "config", "config_default.yml")
         
         with open(config_path, "rb") as yaml_file:
             config = yaml.safe_load(yaml_file)
