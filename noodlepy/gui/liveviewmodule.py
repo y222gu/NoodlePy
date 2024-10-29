d4784ebad60dd1591239d840b38f0d403018c0b3
yifei_from_ml_computer <bmegu@ucdavis.edu>
2024-10-28 18:32:01 -0700

    clean code still in progress

--- noodlepy/gui/liveviewmodule.py
+++ noodlepy/gui/liveviewmodule.py
@@ -22,23 +22,29 @@ from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
 from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
 
 
-class LiveViewCanvas(tk.Canvas):
-    def __init__(self, parent, image_queue, width, height, flip=False):
+class LiveCanvas(tk.Canvas):
+    '''Canvas widget for displaying live images from a camera. 
+    Resizes the image to fit the canvas and draws a crossline in the middle of the image.
+    Converts the image to a PhotoImage object for display.
+    
+    Args:
+        parent: Parent widget
+        image_queue: Queue object for storing images
+        width: Width of the canvas
+        height: Height of the canvas
+        flip: Boolean for flipping the image
+    '''
+    def __init__(self, parent, image_queue, width, height, refresh_rate, flip=False):
         self.image_queue = image_queue
         self._image_width = width
         self._image_height = height
         self._image = None
         self.tk_image = None
         self.flip = flip
+        self.refresh_rate = refresh_rate #ms
         tk.Canvas.__init__(self, parent, width=width, height=height)
         self.grid(row=0, column=0, sticky='nsew')
         self._get_image()
-
-    def _resize(self, event=None):
-        if self._image:
-            resized_image = self._image.resize((self._image_width, self._image_height), Image.LANCZOS)
-            self.tk_image = ImageTk.PhotoImage(resized_image)
-            self.create_image(0, 0, image=self.tk_image, anchor='nw')
 
     def _get_image(self):
         try:
@@ -46,15 +52,37 @@ class LiveViewCanvas(tk.Canvas):
             # the image is mirrored, so we flip it
             if self.flip:
                 self._image = self._image.transpose(Image.FLIP_LEFT_RIGHT)
+
             self._resize()
+            self._draw_crossline()
+            self._display_image()
         except queue.Empty:
             pass
-        self.after(10, self._get_image)
+        self.after(self.refresh_rate, self._get_image)
 
+    def _resize(self, event=None):
+        if self._image:
+            self.tk_image = self._image.resize((self._image_width, self._image_height), Image.LANCZOS)
+
+    def _draw_crossline(self):
+        # Draw middle dash lines on the image
+        draw = ImageDraw.Draw(self.tk_image)
+        draw.line((0, self.tk_image.height / 2, self.tk_image.width, self.tk_image.height / 2), fill='black', width=1)
+        draw.line((self.tk_image.width / 2, 0, self.tk_image.width / 2, self.tk_image.height), fill='black', width=1)
+        del draw
+
+    def _display_image(self):
+        self.tk_image = ImageTk.PhotoImage(self.tk_image)
+        self.create_image(0, 0, image=self.tk_image, anchor='nw')
 
 class ImageAcquisitionThread(threading.Thread):
+    '''Thread for acquiring images from a camera in PIL format and putting them into a queue.
+    
+    Args:
+        camera: TLCamera object
+    '''
     def __init__(self, camera):
-        super(ImageAcquisitionThread, self).__init__()
+        super().__init__()
         camera.exposure_time_us = 40000
         self._camera = camera
         self._previous_timestamp = 0
@@ -71,6 +99,7 @@ class ImageAcquisitionThread(threading.Thread):
         self._stop_event.set()
 
     def _get_image(self, frame):
+        # Process the image frame to convert it to a PIL image
         scaled_image = frame.image_buffer >> (self._bit_depth - 8)
         image8bit = np.asarray(scaled_image, np.uint8)
         processed_image = image8bit.squeeze()
@@ -82,11 +111,6 @@ class ImageAcquisitionThread(threading.Thread):
                 frame = self._camera.get_pending_frame_or_null()
                 if frame is not None:
                     pil_image = self._get_image(frame)
-                    # # draw middle dashlines on the pil_image and put it in the queue
-                    draw = ImageDraw.Draw(pil_image)
-                    draw.line((0, pil_image.height/2, pil_image.width, pil_image.height/2), fill='black', width=5)
-                    draw.line((pil_image.width/2, 0, pil_image.width/2, pil_image.height), fill='black', width=5)
-                    del draw
                     self._image_queue.put(pil_image)
 
             except queue.Full:
@@ -94,7 +118,34 @@ class ImageAcquisitionThread(threading.Thread):
             except Exception as error:
                 print(f"Encountered error: {error}, image acquisition will stop.")
                 break
+        
         print("Image acquisition has stopped")
+
+class CameraController:
+    """Manages camera initialization and acquisition threading."""
+    def __init__(self):
+        # finding all available cameras
+        self._sdk = TLCameraSDK()
+        self._camera_list = self._sdk.discover_available_cameras()
+        self._camera_id = None
+        self._camera = None
+        self._camera_id = camera_id
+        self._sdk = TLCameraSDK()
+        self._camera = self._sdk.open_camera(camera_id)
+        self._camera_thread = ImageAcquisitionThread(self._camera)
+        self._camera_thread.start()
+
+    def get_image_queue(self):
+        return self._camera_thread.get_output_queue()
+
+    def stop_acquisition(self):
+        self._camera_thread.stop()
+        self._camera_thread.join()
+        self._camera.dispose()
+        self._sdk.dispose()
+
+    def __del__(self):
+        self.stop_acquisition()
 
 
 class LiveViewModule(tk.Frame):
@@ -102,14 +153,8 @@ class LiveViewModule(tk.Frame):
         super().__init__(parent)
         self.width = width
         self.height = height
-
-        self.camera_icon = Image.open(os.path.join(os.getcwd(), "noodlepy","assets","camera_icon.png"))
-        self.camera_icon = self.camera_icon.resize((30, 30))
-        self.camera_icon = ImageTk.PhotoImage(self.camera_icon)
-
-        self.exchange_icon = Image.open(os.path.join(os.getcwd(), "noodlepy","assets","exchange_icon.png"))
-        self.exchange_icon = self.exchange_icon.resize((30, 30))
-        self.exchange_icon = ImageTk.PhotoImage(self.exchange_icon)
+        self.camera_icon = self.load_icon(os.path.join(os.getcwd(), "noodlepy","assets","camera_icon.png"))
+        self.exchange_icon = self.load_icon(os.path.join(os.getcwd(), "noodlepy","assets","exchange_icon.png"))
 
         self.sdk = TLCameraSDK()
         camera_list = self.sdk.discover_available_cameras()
@@ -145,7 +190,7 @@ class LiveViewModule(tk.Frame):
         self.live_frame = ttk.Labelframe(self, text="Wide FOV", width=self.width, padding=5)
         self.live_frame.grid(row=0, column=0, sticky='nsew', pady=5, padx=5)
 
-        self.camera_widget = LiveViewCanvas(parent=self.live_frame, image_queue=self.active_camera_thread.get_output_queue(), width=self.width, height=self.height, flip=False)
+        self.camera_widget = LiveCanvas(parent=self.live_frame, image_queue=self.active_camera_thread.get_output_queue(), width=self.width, height=self.height, refresh_rate=10, flip=False)
         self.camera_widget.grid(row=0, column=0, columnspan=2, sticky='nsew')
         self.switch_view_button = ttk.Button(self.live_frame, image = self.exchange_icon, command= lambda: self.switch_view('TO_SMALL'), style='info')
         self.switch_view_button.grid(row=1, column=0, columnspan=1, sticky='nsew', pady=5, padx=5)
@@ -219,6 +264,11 @@ class LiveViewModule(tk.Frame):
         self.column_number_entry.insert(0, "5")
         self.column_number_entry.configure(state=DISABLED)
 
+    def load_icon(self, icon_path):
+        icon = Image.open(icon_path)
+        icon = icon.resize((30, 30))
+        icon = ImageTk.PhotoImage(icon)
+        return icon
 
     def run_in_thread(self, func, *args):
         thread = Thread(target=func, args=args, daemon=True)
