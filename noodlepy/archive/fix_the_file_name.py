ca3f4f1f0bd82d319088211794a8685967453e15
Yifei Gu <yifeigu@yahoo.com>
2024-11-02 15:39:09 -0700

    positional embedding stuff

--- noodlepy/archive/fix_the_file_name.py
+++ noodlepy/archive/fix_the_file_name.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-data_folder = os.path.join(os.getcwd(), "noodlepy", "data", "bec_hnc" )
+data_folder = os.path.join(os.getcwd(), "noodlepy", "data", "bec_hnc", "train")
 
 list_of_file_paths = glob.glob(os.path.join(data_folder, '**/*.txt'), recursive=True)
 
@@ -10,4 +10,7 @@ for file_path in list_of_file_paths:
     if 'redone' in f_split[-1]:
         new_file_name = '_'.join(f_split[:-1])
         new_file_name = new_file_name + '.txt'
+        if os.path.exists(new_file_name):
+            print(f"File already exists: {new_file_name}")
+            os.remove(new_file_name)
         os.rename(file_path, new_file_name)
